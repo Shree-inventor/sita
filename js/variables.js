b7d65b0e0425c0c7a8f510c5b3f20c0daaca6e76
Shree Gujar <87359767+Shree-inventor@users.noreply.github.com>
2024-04-27 09:24:56 +0000

    changes done

--- js/variables.js
+++ js/variables.js
@@ -2,4 +2,5 @@ const accountID = 1234;
 let mailID = 'gujarshri2@gmail.com';
 var city = "mumbai";
 state = "maharashtra";
+// let accountID = 3344;  const cannot be change. 
 console.log (accountID);
